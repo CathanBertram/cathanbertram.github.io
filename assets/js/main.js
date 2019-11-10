82ac880e04d4449b6ff25282cf294783dd39095f
sheepzzxd <55544660+sheepzzxd@users.noreply.github.com>
2019-11-10 16:06:17 +0000

    Additions
    Implemented Slideshow
    Included Modulizer as a game

--- assets/js/main.js
+++ assets/js/main.js
@@ -255,4 +255,30 @@
 
 		}
 
+		var slideIndex = 1;
+		showSlide(slideIndex);
+
+		//Next/Previous Controls
+		function plusSlides(n){
+			showSlide(slideIndex += n);
+		}
+		//thumbnail image control
+		function currentSlide(n){
+			showSlide(slideIndex = n)
+		}
+		function showSlide(n){
+			var i;
+			var slides =document.getElementsByClassName("mySlides");
+			if (n > slides.length) {
+				slideIndex = 1
+			}
+			if (n < 1) {
+				slideIndex=slides.length
+			}
+			for (i = 0; i < slides.length; i++) {
+				slides[i].style.display="none";
+			}
+			slides[slideIndex-1].style.display = "block";
+		}
+
 })(jQuery);
